041ccb7b3f0d1a3912e84944406454273983a0c5
Kalmat <palookjones@gmail.com>
2023-02-01 10:54:19 +0100

    ALL: Added bbox property (x, y, right, bottom), added setParent() method LINUX: Eliminated ewmh and pynput dependencies, added isAlerting property, improved activate() and restore() methods to properly work on Unity, fixed acceptInput() WINDOWS: Added experimental isAlerting property MACOS: Added experimental AcceptInput() method (MacOSNSWindow only)

--- pywinauto/__init__.pyi
+++
@@ -1,6 +0,0 @@
-import sys
-
-assert sys.platform == "win32"  # pyright: ignore[reportInvalidStubStatement]
-
-from .pywinauto import Application as Application
-from .pywinauto import WindowSpecification as WindowSpecification
