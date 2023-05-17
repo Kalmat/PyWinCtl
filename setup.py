88f610414757808e9a013eb499dc76d64486e877
Kalmat <alescribano@gmail.com>
2023-05-17 13:41:01 +0200

    Added new PyMonCtl module to get info and control monitors (for multi-monitor support). Tested OK on Linux and Win32 (macOS pending)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "pywin32>=302; sys_platform == 'win32'",
         "python-xlib>=0.21; sys_platform == 'linux'",
         "pyobjc>=8.1; sys_platform == 'darwin'",
-        "pymonctl>=0.0.2"
+        "pymonctl>=0.0.3"
     ],
     extras_require={
         'dev': [
